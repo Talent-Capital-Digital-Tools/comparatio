009b7f9ec07d4a5bac1592f737b2520e1607ca43
Ali <you@example.com>
2025-09-22 17:31:08 +0300

    init

---
+++ BackEnd/src/main/java/com/example/compa_ratio/CompaRatioApplication.java
@@ -0,0 +1,13 @@
+package com.example.compa_ratio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompaRatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompaRatioApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/example/compa_ratio/CompaRatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.compa_ratio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompaRatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
